ab22b730233f52a5992b23a171878100b74eb9dc
a805431 <gergana.ivanova@yara.com>
2023-04-03 16:35:02 +0300

    s12v139

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,20 +1,29 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
-      <Button title="Add Post" onPress={addBlogPost // the same as () => addBlogPost()
-        }></Button>
+      <Button
+        title="Add Post"
+        onPress={
+          addBlogPost // the same as () => addBlogPost()
+        }
+      ></Button>
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather style={styles.icon} name="trash" />
+            </View>
+          );
         }}
       />
     </View>
@@ -22,6 +31,23 @@ const IndexScreen = () => {
 };
 //useContext() is a function that says 'look at some context object and give us access to that object's value prop right there
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    //flexDirection: row will make the blogpost title and trash icon appear on the same line
+    flexDirection: 'row',
+    //justifyContent will add a bit of space between everything inside our view
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;

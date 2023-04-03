604969a598dcfa08d0ed55731a823f87f4581997
a805431 <gergana.ivanova@yara.com>
2023-04-03 17:24:12 +0300

    s12v141

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,21 +3,35 @@ import createDataContext from '../screens/createDataContext';
 //the state contains the blogPosts
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogpost':
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: 'add_blogpost' });
+  return (id) => {
+    dispatch({ type: 'add_blogpost', payload: id });
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
